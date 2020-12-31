4939e60e37f124e7334ec1062ff906a1f9a3aeee
MadhavRKumar <MadhavRKumar@gmail.com>
2020-12-31 01:34:11 -0500

    package files

--- src/index.js
+++ src/index.js
@@ -1,58 +1,64 @@
 import * as THREE from 'three'
 import { WEBGL } from './webgl'
+import { MeshLine, MeshLineMaterial, MeshLineRaycast } from 'three.meshline'
 
 if (WEBGL.isWebGLAvailable()) {
-  var camera, scene, renderer
-  var mouse,
-    raycaster;
+	var camera, scene, renderer
+	var mouse,
+		raycaster;
 
 
-  var objects = []
 
-  init()
-  render()
+	init();
+	animate();
 
-  function init() {
-    camera = new THREE.PerspectiveCamera(
-      45,
-      window.innerWidth / window.innerHeight,
-      1,
-      10000
-    )
-    camera.position.set(500, 800, 1300)
-    camera.lookAt(0, 0, 0)
+	function init() {
+		camera = new THREE.PerspectiveCamera(
+			45,
+			window.innerWidth / window.innerHeight,
+			1,
+			10000
+		)
+		camera.position.set(500, 800, 1300)
+		camera.lookAt(0, 0, 0)
 
-    scene = new THREE.Scene()
-    scene.background = new THREE.Color(0xf0f0f0)
+		scene = new THREE.Scene()
+		scene.background = new THREE.Color(0x333333)
 
-    raycaster = new THREE.Raycaster()
-    mouse = new THREE.Vector2()
-
-    
-
-    var ambientLight = new THREE.AmbientLight(0x606060)
-    scene.add(ambientLight)
-
-    renderer = new THREE.WebGLRenderer({ antialias: true })
-    renderer.setPixelRatio(window.devicePixelRatio)
-    renderer.setSize(window.innerWidth, window.innerHeight)
-    document.body.appendChild(renderer.domElement)
-
-  }
-
-  function onWindowResize() {
-    camera.aspect = window.innerWidth / window.innerHeight
-    camera.updateProjectionMatrix()
-
-    renderer.setSize(window.innerWidth, window.innerHeight)
-  }
+		raycaster = new THREE.Raycaster()
+		mouse = new THREE.Vector2()
 
 
-  function render() {
-    renderer.render(scene, camera)
-  }
+
+		var ambientLight = new THREE.AmbientLight(0x606060)
+		scene.add(ambientLight)
+
+		renderer = new THREE.WebGLRenderer({ antialias: true })
+		renderer.setPixelRatio(window.devicePixelRatio)
+		renderer.setSize(window.innerWidth, window.innerHeight)
+		document.body.appendChild(renderer.domElement)
+
+		window.addEventListener("resize", onWindowResize, false);
+	}
+
+	function onWindowResize() {
+		camera.aspect = window.innerWidth / window.innerHeight
+		camera.updateProjectionMatrix()
+
+		renderer.setSize(window.innerWidth, window.innerHeight)
+	}
+
+
+	function render() {
+		renderer.render(scene, camera)
+	}
+
+	function animate() {
+		requestAnimationFrame(animate);
+		render();
+	}
 
 } else {
-  var warning = WEBGL.getWebGLErrorMessage()
-  document.body.appendChild(warning)
+	var warning = WEBGL.getWebGLErrorMessage()
+	document.body.appendChild(warning)
 }
